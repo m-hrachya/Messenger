dc0c943d74d47b8f950c121f4baef1c0d75122ff
m-hrachya <hrachyamargaryan2003@gmail.com>
2024-09-09 13:24:00 +0400

    "Added lib.h"

---
+++ lib.h
@@ -0,0 +1,17 @@
+#ifndef LIB
+#define LIB
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <pthread.h>
+#include <signal.h>
+
+#define BUFFER_SIZE 1024
+#define NAME_SIZE 128
+#define MAX_CLIENT_COUNT 10
+
+#endif
